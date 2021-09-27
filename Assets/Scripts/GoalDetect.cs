cbf616c4a10c0ef8c13dbb1104a3e65defd9f5a3
Soori <soorichu@gmail.com>
2021-09-27 14:39:25 +0900

    complete

---
+++ Assets/Scripts/GoalDetect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalDetect : MonoBehaviour
+{
+    [HideInInspector]
+    public PushAgentBase agent;
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("goal"))
+        {
+            agent.ScoredAGoal();
+        }
+    }
+}
---
+++ Assets/Scripts/PushBlockSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class PushBlockSettings : MonoBehaviour
+{
+    public float agentRunSpeed;
+    public float agentRotationSpeed;
+    public float spawnAreaMarginMultiplier;
+    public Material goalScoreMaterial;
+    public Material failMaterial;
+}
